a76e7dcfcde6f7f348a7677f55bdbe6d69476938
Yongcheol Lee <dydcjf8889@gmail.com>
2021-12-02 00:07:49 +0900

    Configure network
    connect punkapi

--- Brewery/BeerListViewController.swift
+++ Brewery/BeerListViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class BeerListViewController: UITableViewController {
     var beerList: [Beer] = []
+    var currentPage = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +20,8 @@ class BeerListViewController: UITableViewController {
         
         tableView.register(BeerListCell.self, forCellReuseIdentifier: "BeerListCell")
         tableView.rowHeight = 150
+        
+        fetchBeer(of: currentPage)
     }
 }
 
@@ -44,5 +47,53 @@ extension BeerListViewController {
         detailVC.beer = selectedBeer
         
         show(detailVC, sender: nil)
+    }
+}
+
+// Data Fetching
+extension BeerListViewController {
+    func fetchBeer(of page: Int) {
+        guard let url = URL(string: "https://api.punkapi.com/v2/beers?page\(page)") else { return }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        let dataTask = URLSession.shared.dataTask(with: request) { [weak self] data, response, error in
+            guard error == nil,
+                  let self = self,
+                  let response = response as? HTTPURLResponse,
+                  let data = data,
+                  let beers = try? JSONDecoder().decode([Beer].self, from: data) else {
+                print("ERROR: URLSession data task \(error?.localizedDescription ?? "")")
+                return
+            }
+            
+            switch response.statusCode {
+            case (200...299): // success
+                self.beerList += beers
+                self.currentPage += 1
+                
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            case (400...499): // client error
+                print("""
+                    ERROR: Client ERROR \(response.statusCode)
+                    Reponse: \(response)
+                """)
+            case (500...599): // server error
+                print("""
+                    ERROR: Server ERROR \(response.statusCode)
+                    Reponse: \(response)
+                """)
+            default:
+                print("""
+                    ERROR: \(response.statusCode)
+                    Reponse: \(response)
+                """)
+            }
+        }
+        
+        dataTask.resume()
     }
 }
